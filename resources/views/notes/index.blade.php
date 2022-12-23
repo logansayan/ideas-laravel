d132fa3a46452ae83c162541b5ca56e0fedb3a87
logansayan <dassayan375@gmail.com>
2022-12-23 11:30:44 +0530

    add sass folder structure

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -1,12 +1,12 @@
 @extends('base')
 
 @section('content')
-    
+  <section class="notes">
   @foreach ($notes as $note)
       <div class="note">
         <h2 class="note__title">{{ $note->title }}</h2>
         <p class="note__body">{{ $note->body }}</p>
       </div>
   @endforeach
-
+  </section>
 @endsection
